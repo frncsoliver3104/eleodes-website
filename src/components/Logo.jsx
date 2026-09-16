422cf635ce00ab8b3a7b2b37aecefaed8ef59335
frncsoliver3104 <villanuevafranz616@gmail.com>
2026-09-17 01:57:31 +0800

    Fix navbar logo sizing and alignment

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import blackImage from '../assets/blackeleodeslogo.png'
 
 const Logo = ({ size = 'md', dark = false, black = false }) => {
   const sizes = {
-    sm: { text: 'text-[2.8rem] md:text-[3.25rem]', icon: 44 },
+    sm: { text: 'text-[2.8rem] md:text-[3.25rem]', icon: 55 },
     md: { text: 'text-[3.6rem] md:text-[4.2rem]', icon: 58 },
     lg: { text: 'text-[4.6rem] md:text-[5.3rem]', icon: 74 },
   }
@@ -15,7 +15,12 @@ const Logo = ({ size = 'md', dark = false, black = false }) => {
   return (
     <div className={`inline-flex items-center whitespace-nowrap ${textColor} ${text} font-normal tracking-[-0.05em] leading-none`}>
       <span>ele</span>
-      <img src={logoImage} alt="o" width={icon} height={icon} className="object-contain -mx-1" />
+      <img
+        src={logoImage}
+        alt="o"
+        width={icon}
+        className="relative left-[calc(0.04em-1.5px)] h-auto shrink-0 object-contain -mx-1"
+      />
       <span>des</span>
     </div>
   )
