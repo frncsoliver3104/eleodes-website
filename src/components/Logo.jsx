115b1f026d9c5e8961032e4d144f4bfa896921cd
frncsoliver3104 <villanuevafranz616@gmail.com>
2026-09-17 01:59:10 +0800

    Align custom logo mark with wordmark

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,12 +3,12 @@ import blackImage from '../assets/blackeleodeslogo.png'
 
 const Logo = ({ size = 'md', dark = false, black = false }) => {
   const sizes = {
-    sm: { text: 'text-[2.8rem] md:text-[3.25rem]', icon: 55 },
-    md: { text: 'text-[3.6rem] md:text-[4.2rem]', icon: 58 },
-    lg: { text: 'text-[4.6rem] md:text-[5.3rem]', icon: 74 },
+    sm: { text: 'text-[2.8rem] md:text-[3.25rem]' },
+    md: { text: 'text-[3.6rem] md:text-[4.2rem]' },
+    lg: { text: 'text-[4.6rem] md:text-[5.3rem]' },
   }
 
-  const { text, icon } = sizes[size] || sizes.md
+  const { text } = sizes[size] || sizes.md
   const textColor = dark ? 'text-white' : 'text-slate-950'
   const logoImage = black ? blackImage : blueImage
 
@@ -18,8 +18,7 @@ const Logo = ({ size = 'md', dark = false, black = false }) => {
       <img
         src={logoImage}
         alt="o"
-        width={icon}
-        className="relative left-[calc(0.04em-1.5px)] h-auto shrink-0 object-contain -mx-1"
+        className="relative mx-[-0.03em] h-[0.88em] w-[0.88em] shrink-0 translate-y-[0.04em] object-contain"
       />
       <span>des</span>
     </div>
